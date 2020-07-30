3630044a98ece2eab08d7cc578d9202752399a56
willile-song <735560190@qq.com>
2020-07-30 16:50:24 +0800

    7.30

--- index.js
+++ index.js
@@ -2,26 +2,15 @@ const http = require("http");
 
 const fs = require('fs');
 
-const url = require('url');
-
-const querystring = require("querystring");
-
 const hostname = "localhost";
 
 const part = 80;
 
 const service = http.createServer((req, res) => {
-    const method = req.method.toLowerCase();
     res.setHeader("Content-Type", "text/html");
     res.statusCode = 200;
-    if(method == "get") {
-        let data = fs.readFileSync("./index.html");
-        res.write(data)
-    }else if(method == "post") {
-        res.on('data', data => {
-            console.log(data.toString())
-        })
-    }
+    let data = fs.readFileSync("./index.html");
+    res.write(data)
     res.end();
 })
 
